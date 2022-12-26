b49bfbfb27ac782cb31f8d090336f652c8c829f9
iLynette <lynetteacholah@icloud.com>
2022-12-26 18:51:00 +0300

    Migration files

---
+++ db/migrate/20221226134536_create_appointments.rb
@@ -0,0 +1,14 @@
+class CreateAppointments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :appointments do |t|
+      t.string :title
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :location
+      t.integer :duration
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

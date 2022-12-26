0616161de17b8ce6cc5a4af5fee2c0372693e512
iLynette <lynetteacholah@icloud.com>
2022-12-26 18:51:25 +0300

    Appointment and bookings controllers

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,51 @@
+class AppointmentsController < ApplicationController
+    before_action :set_appointment, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!
+  
+    def index
+      @appointments = current_user.appointments
+    end
+  
+    def show
+    end
+  
+    def new
+      @appointment = Appointment.new
+    end
+  
+    def edit
+    end
+  
+    def create
+      @appointment = current_user.appointments.build(appointment_params)
+  
+      if @appointment.save
+        redirect_to @appointment, notice: 'Appointment was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def update
+      if @appointment.update(appointment_params)
+        redirect_to @appointment, notice: 'Appointment was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @appointment.destroy
+      redirect_to appointments_url, notice: 'Appointment was successfully destroyed.'
+    end
+  
+    private
+      def set_appointment
+        @appointment = current_user.appointments.find(params[:id])
+      end
+  
+      def appointment_params
+        params.require(:appointment).permit(:start_time, :end_time, :duration, :location, :notes)
+      end
+  end
+  

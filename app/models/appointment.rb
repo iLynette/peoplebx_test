79ced23579486a1e3098030ce84ce93befc08177
iLynette <lynetteacholah@icloud.com>
2022-12-29 22:21:24 +0300

    Include options and slots in model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,9 +1,27 @@
 class Appointment < ApplicationRecord
+
+  OPTIONS = {
+    "15": 15,
+    "30": 30,
+    "45": 45,
+    "60": 60,
+    "90": 90
+  }
+
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
   validates :start_time, presence: true
   validates :end_time, presence: true
-  validates :duration, presence: true
   validates :location, presence: true
+
+
+  before_validation do
+    self.end_time = self.start_time + 1.hour
+  end
+
+
+  def self.slots(user)
+    (Date.today.beginning_of_day.to_datetime.to_i..Date.today.end_of_day.to_datetime.to_i).step(user.appointment_duration.minutes).map { Time.at(_1) }
+  end
 end

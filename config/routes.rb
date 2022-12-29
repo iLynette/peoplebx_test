67520d159304615c819e430d3e9c6ed46e87b727
iLynette <lynetteacholah@icloud.com>
2022-12-29 22:23:07 +0300

    Modify views and routes, run migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   resources :appointments do
     resources :bookings
-    get :available_slots, on: :collection
+    # get :available_slots, on: :collection
+    # resource :available_slots, only: [:show], controller: :available_slots
   end
+
+  get "book/*email", to: "available_slots#show"
+
+  resource :appointment_duration, only: [:create], controller: :appointment_duration
 
   devise_for :users
 
-  root to: "appointments#index"
+  root to: "available_slots#show"
+
+  # root to: "appointments#index"
 end

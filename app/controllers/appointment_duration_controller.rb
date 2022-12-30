0702f9581737f301ddcda0b32fa9ce2c9164971a
iLynette <lynetteacholah@icloud.com>
2022-12-30 23:07:30 +0300

    Change redirect path and remove redundant code

--- app/controllers/appointment_duration_controller.rb
+++ app/controllers/appointment_duration_controller.rb
@@ -2,6 +2,6 @@ class AppointmentDurationController < ApplicationController
     def create
       current_user.update!(appointment_duration: params[:duration])
   
-      redirect_to root_path, notice: "Duration was updated"
+      redirect_to '/book/*email', notice: "Duration was updated"
     end
   end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -9,8 +9,6 @@ class Appointment < ApplicationRecord
   }
 
   belongs_to :user
-  has_many :bookings, dependent: :destroy
-
   validates :start_time, presence: true
   validates :end_time, presence: true
   validates :location, presence: true

afb03cb99945ec409ce5a4024159af77c9b6d17e
iLynette <lynetteacholah@icloud.com>
2023-01-11 08:20:46 +0300

    Clean code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,9 +8,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user = User.create!(name: "Admin", surname: "mimi", email: "mimi@gmail.com", password: "123456")
-Appointment.create!(user: user,start_time: "2021-05-01 10:00:00", end_time: "2021-05-01 11:00:00", duration: 90, location: "Paris", title: "random")
-Appointment.create!(user: user,start_time: "2021-05-01 10:00:00", end_time: "2021-05-01 11:00:00", duration: 60, location: "Paris", title: "random")
-Appointment.create!(user: user,start_time: "2021-05-01 10:00:00", end_time: "2021-05-01 11:00:00", duration: 45, location: "Paris", title: "random")
-Appointment.create!(user: user,start_time: "2021-05-01 10:00:00", end_time: "2021-05-01 11:00:00", duration: 15, location: "Paris", title: "random")
-Appointment.create!(user: user,start_time: "2021-05-01 10:00:00", end_time: "2021-05-01 11:00:00", duration: 30, location: "Paris", title: "random")
+user = User.create!(name: 'Admin', surname: 'mimi', email: 'mimi@gmail.com', password: '123456')
+Appointment.create!(user: user, start_time: '2021-05-01 10:00:00', end_time: '2021-05-01 11:00:00', duration: 90,
+                    location: 'Paris', title: 'random')
+Appointment.create!(user: user, start_time: '2021-05-01 10:00:00', end_time: '2021-05-01 11:00:00', duration: 60,
+                    location: 'Paris', title: 'random')
+Appointment.create!(user: user, start_time: '2021-05-01 10:00:00', end_time: '2021-05-01 11:00:00', duration: 45,
+                    location: 'Paris', title: 'random')
+Appointment.create!(user: user, start_time: '2021-05-01 10:00:00', end_time: '2021-05-01 11:00:00', duration: 15,
+                    location: 'Paris', title: 'random')
+Appointment.create!(user: user, start_time: '2021-05-01 10:00:00', end_time: '2021-05-01 11:00:00', duration: 30,
+                    location: 'Paris', title: 'random')
